00549d1af792b8e619f3e23d62f1d8bc40294b2c
jscontrerasp <jscontrerasp@unal.edu.co>
2022-02-15 06:08:46 -0500

    DbContext and configure Service in Program.cs

---
+++ DapperCourse/Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using DapperCourse.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DapperCourse.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Company> Companies { get; set; } 
+    }
+}
---
+++ DapperCourse/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace DapperCourse.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
